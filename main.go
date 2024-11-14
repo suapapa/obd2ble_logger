038c7e59045758238ebe480354dabf59d7373952
Homin Lee <ff4500@gmail.com>
2024-11-14 23:59:10 +0900

    catch ctrl-c

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"encoding/json"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/go-ble/ble"
@@ -29,28 +32,37 @@ func main() {
 	}
 	defer elm327ble.Close()
 
+	// Receive Ctrl+C signal
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
 	tk := time.NewTicker(500 * time.Millisecond)
 	defer tk.Stop()
-	for range tk.C {
-		vs, err := elm327ble.ReadCurrDatas(
-			[]byte{OBD2Svc01EngineSpeed, OBD2Svc01VehicleSpeed},
-		)
-		if err != nil {
-			log.Printf("Failed to read current datas: %v", err)
-			continue
-		}
-
-		var r Report
-		r.UTC = time.Now().UnixMilli()
-		for _, v := range vs {
-			switch v.Pid {
-			case OBD2Svc01EngineSpeed:
-				r.EngineSpeed = float64(256*int(v.Data[0])+int(v.Data[1])) / 4
-			case OBD2Svc01VehicleSpeed:
-				r.VehicleSpeed = int(v.Data[0])
+	for {
+		select {
+		case <-sigs:
+			return
+		case <-tk.C:
+			vs, err := elm327ble.ReadCurrDatas(
+				[]byte{OBD2Svc01EngineSpeed, OBD2Svc01VehicleSpeed},
+			)
+			if err != nil {
+				log.Printf("Failed to read current datas: %v", err)
+				continue
 			}
+
+			var r Report
+			r.UTC = time.Now().UnixMilli()
+			for _, v := range vs {
+				switch v.Pid {
+				case OBD2Svc01EngineSpeed:
+					r.EngineSpeed = float64(256*int(v.Data[0])+int(v.Data[1])) / 4
+				case OBD2Svc01VehicleSpeed:
+					r.VehicleSpeed = int(v.Data[0])
+				}
+			}
+			jsonStr, _ := json.Marshal(r)
+			log.Printf("%s", jsonStr)
 		}
-		jsonStr, _ := json.Marshal(r)
-		log.Printf("%s", jsonStr)
 	}
 }
